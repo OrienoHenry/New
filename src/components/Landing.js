6764b14ba49c16cc43bb003b68c1e45b3eb5ed67
alabi sodiq <adisco4420@gmail.com>
2019-03-18 22:30:51 +0100

    error fix

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import './../App.css';
 
 
 
 
-class Home extends Component {
+class Landing extends Component {
     render() {
         return(
             <React.Fragment>
